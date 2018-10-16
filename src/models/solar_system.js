a3e6d55f3a5cd2bc00ea1c27719dc3f744a3e881
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-10-16 16:21:32 +0100

    Finished paired lab with Jordan

---
+++ src/models/solar_system.js
@@ -0,0 +1,19 @@
+const PubSub = require('../helpers/pub_sub.js')
+
+const SolarSystem = function(planets) {
+  this.planets = planets;
+};
+
+SolarSystem.prototype.bindEvents = function() {
+  PubSub.subscribe('SelectView:clickPlanet', (event) => {
+    const planetId = event.detail;
+
+    const selectedPlanet = this.planets.filter((planet) =>{
+      return planet.name === planetId
+    });
+
+    PubSub.publish('SolarSystem:selected-planet-ready', selectedPlanet)
+  })
+}
+
+module.exports = SolarSystem;
---
+++ src/views/planet_info_view.js
@@ -0,0 +1,29 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const PlanetInfoView = function(container){
+  this.container = container;
+};
+
+PlanetInfoView.prototype.bindEvents = function(){
+  PubSub.subscribe('SolarSystem:selected-planet-ready', (event) =>{
+    const selectedPlanet = event.detail[0];
+    this.render(selectedPlanet);
+  });
+};
+
+PlanetInfoView.prototype.render = function(planet){
+  
+  const infoParagraph = document.createElement('p');
+  infoParagraph.textContent = `The planet ${planet.name} has an orbit of ${planet.orbit}, a day of ${planet.day}
+   a surface area of ${planet.surfaceArea}, a volume of ${planet.volume}, a gravity of ${planet.gravity} and has ${planet.moons} moons.`
+  this.container.innerHTML = '';
+  this.container.appendChild(infoParagraph);
+
+  const img = document.createElement("img");
+  img.src = planet.image;
+  img.alt = `image of ${planet.name}`
+  img.id = 'planetImg';
+  this.container.appendChild(img)
+};
+
+module.exports = PlanetInfoView;
